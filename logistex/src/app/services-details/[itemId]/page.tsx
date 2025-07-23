400bc2250498f6630e38a071aa48289a1c30a2fd
AaA <awarageochemistry@gmail.com>
2025-07-23 19:50:00 +0300

    Fix image-18

--- logistex/src/app/services-details/[itemId]/page.tsx
+++ logistex/src/app/services-details/[itemId]/page.tsx
@@ -1,15 +1,35 @@
 import ServiceDetails from "@/components/pages/services/service-details"
+import service_data from "@/data/ServiceData"
 import Wrapper from "@/layouts/Wrapper"
 
 export const metadata = {
   title:
     "Service Details Logistex - Transport & Logistics React Next js Template",
 }
-const page = () => {
-  console.log("hi-1 inside folder of service-details")
+
+export async function generateStaticParams() {
+  return service_data.map((item) => ({
+    itemId: item.id.toString(), // must match the dynamic param name
+  }))
+}
+
+// const page = () => {
+//   console.log("hi-1 inside folder of service-details")
+
+//   return (
+//     <Wrapper>
+//       <ServiceDetails />
+//     </Wrapper>
+//   )
+// }
+
+// export default page
+const page = ({ params }: { params: { itemId: string } }) => {
+  console.log("hi-1 inside folder of service-details", params.itemId)
+
   return (
     <Wrapper>
-      <ServiceDetails />
+      <ServiceDetails itemId={params.itemId} />
     </Wrapper>
   )
 }
--- logistex/src/components/pages/services/service-details/index.tsx
+++ logistex/src/components/pages/services/service-details/index.tsx
@@ -4,21 +4,48 @@ import HeaderFour from "@/layouts/headers/HeaderFour"
 import ServiceDetailsArea from "./ServiceDetailsArea"
 // import CTA from "../../who-we-are/CTA"
 import FooterTwo from "@/layouts/footers/FooterTwo"
+import service_data from "@/data/ServiceData"
 
-const ServiceDetails = () => {
-  console.log("hi inside folder of service-details, then index")
+interface ServiceDetailsProps {
+  itemId: string
+}
+
+const ServiceDetails = ({ itemId }: ServiceDetailsProps) => {
+  console.log("inside ServiceDetails, itemId:", itemId)
+
+  const item = service_data.find((data) => data.id === Number(itemId))
+
+  if (!item) return <p>Service not found.</p>
+
   return (
     <div className="theme-red">
       <ScrollToTop />
       <HeaderFour />
       <main className="fix">
-        <Breadcumb title="Service Details" sub_title="Service Details" />
-        <ServiceDetailsArea />
+        <Breadcumb title={item.title} sub_title="Service Details" />
+        <ServiceDetailsArea item={item} />
       </main>
-      {/* <CTA /> */}
       <FooterTwo style={true} />
     </div>
   )
 }
 
 export default ServiceDetails
+
+// const ServiceDetails = () => {
+//   console.log("hi inside folder of service-details, then index")
+//   return (
+//     <div className="theme-red">
+//       <ScrollToTop />
+//       <HeaderFour />
+//       <main className="fix">
+//         <Breadcumb title="Service Details" sub_title="Service Details" />
+//         <ServiceDetailsArea />
+//       </main>
+//       {/* <CTA /> */}
+//       <FooterTwo style={true} />
+//     </div>
+//   )
+// }
+
+// export default ServiceDetails
