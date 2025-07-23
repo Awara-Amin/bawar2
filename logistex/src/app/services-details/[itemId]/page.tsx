12a20a16684e09301feaca8304b3aed438f9fa0d
AaA <awarageochemistry@gmail.com>
2025-07-24 00:43:41 +0300

    Fix image-25

--- logistex/src/app/services-details/[itemId]/page.tsx
+++ logistex/src/app/services-details/[itemId]/page.tsx
@@ -21,17 +21,15 @@ export const metadata = {
 // export default page
 
 // ✅ Generates static paths for each item
-// export function generateStaticParams() {
-//   return service_data.map((item) => ({
-//     itemId: item.id.toString(),
-//   }))
-// }
+export function generateStaticParams() {
+  return service_data.map((item) => ({
+    itemId: item.id,
+  }))
+}
 
 // ✅ Correct typing of props here (no async type confusion)
-export default function Page({ params }: { params: { itemId: string } }) {
-  const item = service_data.find(
-    (service) => service.id.toString() === params.itemId
-  )
+export default function Page({ params }: { params: { itemId: number } }) {
+  const item = service_data.find((service) => service.id === params.itemId)
 
   if (!item) return notFound()
 
